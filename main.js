a9f08329fdcd1202ffdfbfd5bf4c787286d4b032
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-05-20 05:59:51 +0000

    versión 'mobile' terminada

--- main.js
+++ main.js
@@ -1,2 +1,67 @@
 import './normalize.css'
 import './index.scss'
+
+import { data } from './times.data'
+
+const options = document.querySelector('div.profile-options')?.children
+const cards = document.querySelector('section.cards')
+
+let current_pos
+
+// [?] actualizar elementos en el DOM
+const loadElements = (pos) => {
+  if(current_pos == pos) return cards.children
+  current_pos = pos
+
+  console.log('[+] loadElements()')
+  cards.innerHTML = ''
+  
+  for(let dat of data) {
+    const element = `
+      <div class='card ${ dat.name }'>
+        <div class='card-container'>
+          <h2>${ dat.name }</h2>
+          <div class='menu'></div>
+          <div class='data-times'>
+            <p class='current-time'>${ dat.data_times[pos].current }</p>
+            <p class='previous-time'>${ dat.data_times[pos].previous }</p>
+          </div>
+        </div>
+      </div>
+    `
+    cards.innerHTML += element
+  }
+
+  return cards.children
+}
+
+// [?] cargar eventos en opciones
+const loadEvents = () => {
+  console.log('[+] loadEvents()')
+
+  for(let i = 0; i < options.length; i++) {
+    options[i].onclick = () => handleEvent(i)
+  }
+}
+
+// [?] evento a ejecutar en cada opción
+const handleEvent = (option) => {
+  console.log(`[+] handleEvent() in ${ option } option`)
+
+  for(let i = 0; i < options.length; i++) {
+    if(i == option){
+      options[i].classList.add('active')
+      loadElements(option)
+    }
+    else
+      options[i].classList.remove('active')
+  }
+}
+
+// [*] START
+
+// cargar eventos
+loadEvents()
+
+// * iniciar elementos
+loadElements(0)
---
+++ times.data.js
@@ -0,0 +1,104 @@
+export const data = [
+  {
+    name: 'Work',
+    data_times: [
+      {
+        current: '5hrs',
+        previous: 'Last Day - 7hrs'
+      },
+      {
+        current: '32hrs',
+        previous: 'Last Week - 36hrs'
+      },
+      {
+        current: '103hrs',
+        previous: 'Last Month - 128hrs'
+      }
+    ]
+  },
+  {
+    name: 'Play',
+    data_times: [
+      {
+        current: '1hr',
+        previous: 'Last Day - 2hrs'
+      },
+      {
+        current: '10hrs',
+        previous: 'Last Week - 8hrs'
+      },
+      {
+        current: '23hrs',
+        previous: 'Last Month - 29hrs'
+      }
+    ]
+  },
+  {
+    name: 'Study',
+    data_times: [
+      {
+        current: '0hrs',
+        previous: 'Last Day - 1hr'
+      },
+      {
+        current: '4hrs',
+        previous: 'Last Week - 7hrs'
+      },
+      {
+        current: '13hrs',
+        previous: 'Last Month - 19hrs'
+      }
+    ]
+  },
+  {
+    name: 'Exercise',
+    data_times: [
+      {
+        current: '1hr',
+        previous: 'Last Day - 1hr'
+      },
+      {
+        current: '4hrs',
+        previous: 'Last Week - 5hrs'
+      },
+      {
+        current: '11hrs',
+        previous: 'Last Month - 18hrs'
+      }
+    ]
+  },
+  {
+    name: 'Social',
+    data_times: [
+      {
+        current: '1hr',
+        previous: 'Last Day - 3hrs'
+      },
+      {
+        current: '5hrs',
+        previous: 'Last Week - 10hrs'
+      },
+      {
+        current: '21hrs',
+        previous: 'Last Month - 23hrs'
+      }
+    ]
+  },
+  {
+    name: 'Self Care',
+    data_times: [
+      {
+        current: '0hrs',
+        previous: 'Last Day - 1hr'
+      },
+      {
+        current: '2hrs',
+        previous: 'Last Week - 2hrs'
+      },
+      {
+        current: '7hrs',
+        previous: 'Last Month - 11hrs'
+      }
+    ]
+  }
+]
